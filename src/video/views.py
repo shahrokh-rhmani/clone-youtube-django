3de6bc06ed6b61bdb1fba937559f8816a9ae8de5
shah <shahrokh.r1379@gmail.com>
2024-09-08 20:12:48 +0330

    Add listview function, base and nav templates, and static files setup

--- src/video/views.py
+++ src/video/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Video
 
-# Create your views here.
+
+def listview(request):
+    most_recent_videos = Video.objects.order_by('-datetime')[:8]
+
+    context = {
+        'most_recent_videos': most_recent_videos,
+    }
+    
+    return render(request, 'listview.html', context)
