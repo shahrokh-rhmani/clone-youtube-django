57e1eaee930ff8d84a1c24fb29f386b91fbea342
shah <shahrokh.r1379@gmail.com>
2024-09-08 16:47:39 +0330

    Add Channel model and ChannelSubscription model

--- src/studio/models.py
+++ src/studio/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    user = models.ForeignKey(User, related_name="channels", on_delete=models.CASCADE)
+    channel_name = models.CharField(max_length=50)
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.channel_name
+    
+    def count_subscribers(self):
+        return self.subscriptions.count()
+    
+    
+class ChannelSubscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, related_name="subscriptions", on_delete=models.CASCADE)
