eb2ffa556769e2bc8969e696a812b56388438845
shah <shahrokh.r1379@gmail.com>
2024-09-08 18:43:28 +0330

    Add Video model

--- src/video/models.py
+++ src/video/models.py
@@ -1,3 +1,25 @@
+from PIL import Image
 from django.db import models
+from studio.models import Channel
 
-# Create your models here.
+
+class Video(models.Model):
+    channel = models.ForeignKey(Channel, null=True, blank=True, related_name="videos", on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    path = models.URLField(max_length=200, null=True, blank=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    number_of_views = models.BigIntegerField(default=0)
+    img = models.ImageField(upload_to='videos/images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.img:
+            img = Image.open(self.img.path)
+            output_size = (625, 350)  # Set desired width and height
+            img = img.resize(output_size, Image.LANCZOS)
+            img.save(self.img.path)
