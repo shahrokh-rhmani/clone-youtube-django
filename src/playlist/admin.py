5c01e3ba775fea3f1da733d12cdffe149641f13e
shah <shahrokh.r1379@gmail.com>
2024-10-17 12:51:54 +0330

    Add Playlist and PlaylistItem models

--- src/playlist/admin.py
+++ src/playlist/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Playlist, PlaylistItem
 
-# Register your models here.
+admin.site.register(Playlist)
+admin.site.register(PlaylistItem)
--- src/playlist/models.py
+++ src/playlist/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from video.models import Video
 
-# Create your models here.
+
+class Playlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PlaylistItem(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, related_name="playlist_items", on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['playlist', 'video'], name='unique_playlist_video')
+        ]
+
+    def __str__(self):
+        return f'{self.playlist.title} - {self.video.title}'
+
